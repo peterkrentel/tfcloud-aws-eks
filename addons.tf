2d2cd66cf9223bb52cd6285012297ac7521cdaae
peterkrentel <37800830+peterkrentel@users.noreply.github.com>
2025-06-15 13:20:18 +0000

    addon for lb

--- addons.tf
+++ addons.tf
@@ -1,19 +1,6 @@
-module "eks_blueprints_addons" {
-  source  = "aws-ia/eks-blueprints-addons/aws"
-  version = "1.21.0"
+module "load_balancer_addon" {
+  source = "git::https://github.com/aws-samples/eks-workshop-v2.git//manifests/modules/exposing/load-balancer/.workshop/terraform?ref=stable"
 
-  cluster_name      = var.addon_context.eks_cluster_id
-  cluster_endpoint  = var.addon_context.aws_eks_cluster_endpoint
-  cluster_version   = var.eks_cluster_version
-  oidc_provider_arn = var.addon_context.eks_oidc_provider_arn
-
-  enable_aws_load_balancer_controller = true
-  aws_load_balancer_controller = {
-    role_name   = "${var.addon_context.eks_cluster_id}-alb-controller"
-    policy_name = "${var.addon_context.eks_cluster_id}-alb-controller"
-  }
-
-  create_kubernetes_resources = false
-
-  observability_tag = null
+  cluster_name = module.eks.cluster_name
+  region       = var.region
 }
