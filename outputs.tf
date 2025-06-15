660a080924ecf60b0149ee3bb62e4368c181af1e
peterkrentel <37800830+peterkrentel@users.noreply.github.com>
2025-06-15 14:21:49 +0000

    start over

--- outputs.tf
+++
@@ -1,8 +0,0 @@
-output "environment_variables" {
-  description = "Environment variables to be added to the IDE shell"
-  value = {
-    LBC_CHART_VERSION = var.load_balancer_controller_chart_version
-    LBC_ROLE_ARN      = module.eks_blueprints_addons.aws_load_balancer_controller.iam_role_arn
-  }
-}
-
