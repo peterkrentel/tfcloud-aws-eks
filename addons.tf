025faacee67fcd45d13ba5a5eadb9b19876edfed
peterkrentel <37800830+peterkrentel@users.noreply.github.com>
2025-06-16 20:25:04 +0000

    modify reference to eks module

--- addons.tf
+++ addons.tf
@@ -2,15 +2,15 @@ module "eks_blueprints_addons" {
   source  = "aws-ia/eks-blueprints-addons/aws"
   version = "1.21.0"
 
-  cluster_name      = var.addon_context.eks_cluster_id
-  cluster_endpoint  = var.addon_context.aws_eks_cluster_endpoint
-  cluster_version   = module.eks_cluster_version
-  oidc_provider_arn = var.addon_context.eks_oidc_provider_arn
+  cluster_name      = module.eks.eks_cluster_id
+  cluster_endpoint  = module.eks.aws_eks_cluster_endpoint
+  cluster_version   = module.eks.eks_cluster_version
+  oidc_provider_arn = module.eks.eks_oidc_provider_arn
 
   enable_aws_load_balancer_controller = true
   aws_load_balancer_controller = {
-    role_name   = "${var.addon_context.eks_cluster_id}-alb-controller"
-    policy_name = "${var.addon_context.eks_cluster_id}-alb-controller"
+    role_name   = "${module.eks.cluster_name}-alb-controller"
+    policy_name = "${module.eks.cluster_name}-alb-controller"
   }
 
   create_kubernetes_resources = false
--- terraform.tfvars
+++
@@ -1,5 +0,0 @@
-addon_context = {
-  eks_cluster_id           = module.eks.cluster_name
-  aws_eks_cluster_endpoint = module.eks.cluster_endpoint
-  eks_oidc_provider_arn    = module.eks.oidc_provider_arn
-}
