139003ed4be89618268462b3d9bb2a188337a979
peterkrentel <37800830+peterkrentel@users.noreply.github.com>
2025-06-16 20:40:34 +0000

    cluster name tweak

--- addons.tf
+++ addons.tf
@@ -2,7 +2,7 @@ module "eks_blueprints_addons" {
   source  = "aws-ia/eks-blueprints-addons/aws"
   version = "1.21.0"
 
-  cluster_name      = module.eks_cluster_id
+  cluster_name      = module.eks.cluster_name
   cluster_endpoint  = module.eks.aws_eks_cluster_endpoint
   cluster_version   = module.eks.cluster_version
   oidc_provider_arn = module.eks.eks_oidc_provider_arn
