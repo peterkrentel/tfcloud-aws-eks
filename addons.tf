e691f1c9e9c96d7257e28af7bfb7d4f4a40e5024
peterkrentel <37800830+peterkrentel@users.noreply.github.com>
2025-06-16 20:57:11 +0000

    fingers crossed

--- addons.tf
+++ addons.tf
@@ -3,7 +3,7 @@ module "eks_blueprints_addons" {
   version = "1.21.0"
 
   cluster_name      = module.eks.cluster_name
-  cluster_endpoint  = module.eks.aws_eks_cluster_endpoint
+  cluster_endpoint  = module.eks.cluster_endpoint
   cluster_version   = module.eks.cluster_version
   oidc_provider_arn = module.eks.oidc_provider_arn
 
