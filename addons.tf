503cbd0d00bdbe6d8221c032fb8bc7362601d854
peterkrentel <37800830+peterkrentel@users.noreply.github.com>
2025-06-16 20:19:03 +0000

    cluster version remove from tfvars

--- addons.tf
+++ addons.tf
@@ -4,7 +4,7 @@ module "eks_blueprints_addons" {
 
   cluster_name      = var.addon_context.eks_cluster_id
   cluster_endpoint  = var.addon_context.aws_eks_cluster_endpoint
-  cluster_version   = var.eks_cluster_version
+  cluster_version   = module.eks_cluster_version
   oidc_provider_arn = var.addon_context.eks_oidc_provider_arn
 
   enable_aws_load_balancer_controller = true
--- terraform.tfvars
+++ terraform.tfvars
@@ -3,5 +3,3 @@ addon_context = {
   aws_eks_cluster_endpoint = module.eks.cluster_endpoint
   eks_oidc_provider_arn    = module.eks.oidc_provider_arn
 }
-
-eks_cluster_version = module.eks.cluster_version   # or a literal "1.30"
